f492e2c10056a67d8b0e23d5cba9090c90f58adf
MNH225 <1044138@mborijnland.nl>
2023-01-23 00:00:17 +0100

    Create index.blade.php

---
+++ oefenen1/resources/views/notes/index.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            @foreach ($notes as $note)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    <h2 class="font-bold text-2xl">
+                        {{ $note->title }}
+                    </h2>
+                    <p class="mt-2">
+                        {{ Str::limit($note->text, 200) }}
+                    </p>
+                    <span>
+                        {{ $note->updated_at->diffForHumans() }}
+                    </span>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+</x-app-layout>
